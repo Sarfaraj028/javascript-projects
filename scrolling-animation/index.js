edf417b6e7a28894b1d6954d4878aa5e046443e2
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-11-05 10:06:47 +0530

    finnaly the project is working as per the expectations

--- scrolling-animation/index.js
+++ scrolling-animation/index.js
@@ -1,13 +1,14 @@
 const boxes = document.querySelectorAll(".container");
 
 window.addEventListener("scroll", () => {
-    const windowInnerHeight = window.innerHeight;
+    const windowInnerHeight = window.innerHeight; //window.innerHeight = 738px
 
     boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
+        const boxTop = box.getBoundingClientRect().top // this method get the element position and size of the elemeents here from the top
 
         
-        if(boxTop < windowInnerHeight){
+        if(boxTop < windowInnerHeight){ // here when we scroll and the elennt comes from the bottom to top then the element position will decrease and 
+                                        // a time it will rich less than innerHeight then then the event will occur and the element would be animated
             box.classList.add("show")
         }
         else{

192d8ecbfe5add1ec5a47cf34f590d9c97ba7b07
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-11-08 18:01:27 +0530

    A social media links that pop-up after hovering on that project is added

---
+++ social-media-links/index.js
@@ -0,0 +1,51 @@
+// Facebook
+
+const fbELe = document.querySelector(".fb")
+
+function displayFB(){
+    fbELe.classList.add("show")
+    
+}
+function hideFB(){
+    fbELe.classList.remove("show")
+}
+
+//Twitter 
+const twiELe = document.querySelector(".twi")
+function displayTwi(){
+    twiELe.classList.add("show-twi")
+    
+}
+function hideTwi(){
+    twiELe.classList.remove("show-twi")
+}
+
+//Instagram
+const instaELe = document.querySelector(".insta")
+function displayInsta(){
+    instaELe.classList.add("show-insta")
+    
+}
+function hideInsta(){
+    instaELe.classList.remove("show-insta")
+}
+
+//Github
+const gitELe = document.querySelector(".git")
+function displayGit(){
+    gitELe.classList.add("show-git")
+    
+}
+function hideGit(){
+    gitELe.classList.remove("show-git")
+}
+
+// Youtube
+const ytELe = document.querySelector(".yt")
+function displayYt(){
+    ytELe.classList.add("show-yt")
+    
+}
+function hideYt(){
+    ytELe.classList.remove("show-yt")
+}

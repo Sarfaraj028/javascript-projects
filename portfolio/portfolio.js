a2bb2f756e481eaa77f3b1a50771ae38e556869a
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-12-09 10:43:13 +0530

    Some updates in the project

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,10 +1,20 @@
-/* let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('nav');
 
-menuIcon.onClick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}; */
+// active nav bar
+const navLinks = document.querySelectorAll('li a');
+console.log(navLinks);
+
+// Loop through each link and add event listener
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    // Remove 'active' class from all links
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    // Add 'active' class to the clicked link
+    this.classList.add('active');
+  });
+});
+
+
 function toggleMenu() {
     var navLink = document.getElementById("navLinks");
     if (navLink.style.display === "block") {
